d75395967ab844a58ede83422f33b890ed92b475
bobbybish81 <bobbybish81@hotmail.com>
2023-05-17 10:47:51 +0200

    start script modified

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ import loginRouter from './routes/login.js';
 import logoutRouter from './routes/logout.js';
 import resetRouter from './routes/resetPassword.js';
 import verifyRouter from './routes/verifyEmail.js';
+import cors from 'cors';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -16,12 +17,7 @@ const port = process.env.port || 8080;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://quizzierascal.netlify.app');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
+app.use(cors());
 
 // eslint-disable-next-line no-undef
 const uri = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}${process.env.MONGO_CLUSTER}/${process.env.MONGO_DBNAME}?retryWrites=true&w=majority`;
