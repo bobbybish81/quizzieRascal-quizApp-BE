79e2de0eecfae8db3b9a604406f8f9d30ee46a7a
bobbybish81 <bobbybish81@hotmail.com>
2023-05-17 10:40:40 +0200

    set headers for Access Control

--- src/app.js
+++ src/app.js
@@ -7,7 +7,6 @@ import loginRouter from './routes/login.js';
 import logoutRouter from './routes/logout.js';
 import resetRouter from './routes/resetPassword.js';
 import verifyRouter from './routes/verifyEmail.js';
-import cors from 'cors';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -17,7 +16,12 @@ const port = process.env.port || 8080;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://quizzierascal.netlify.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 // eslint-disable-next-line no-undef
 const uri = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}${process.env.MONGO_CLUSTER}/${process.env.MONGO_DBNAME}?retryWrites=true&w=majority`;
