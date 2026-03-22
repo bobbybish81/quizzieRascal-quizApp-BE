c6ae0ea67a7335dda5aacabfee7e712cffacb6b1
bobbybish81 <bobbybish81@hotmail.com>
2026-03-22 22:41:32 +0100

    Fix error handling

--- src/app.js
+++ src/app.js
@@ -47,7 +47,8 @@ app.use(express.urlencoded({ extended: false }));
 const uri = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}${process.env.MONGO_CLUSTER}/${process.env.MONGO_DBNAME}?retryWrites=true&w=majority`;
 
 mongoose.connect(uri)
-  .then(() => console.log("Connected to MongoDB"))
+  // .then(() => console.log("Connected to MongoDB"))
+  .then(() => console.log("Connected successfully"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
 // Routes
